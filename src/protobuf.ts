267d5efef9e7d91fa278a2aeb872d6913a29d06f
Egor Koleda <radioegor146@ctf.by>
2026-04-19 05:16:07 +0300

    fix: protobuf

--- src/protobuf.ts
+++ src/protobuf.ts
@@ -12,8 +12,7 @@ function decodeProtobufFieldValue(value: any): any {
         return !!value.boolValue;
     }
     if ("listValue" in value) {
-        console.info(value)
-        return value.listValue.map((item: any) => decodeProtobufFieldValue(item))
+        return value.listValue.values.map((item: any) => decodeProtobufFieldValue(item))
     }
     if ("numberValue" in value) {
         return value.numberValue;
